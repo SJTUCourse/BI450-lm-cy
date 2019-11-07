934df23fd33f8c9f02983f22e8be35e7be847361
SelinaLi11 <244079364@qq.com>
2019-11-07 14:14:06 +0800

    update project 1

--- Project 1/src/GUI/BioFailed.m
+++
@@ -1,7 +0,0 @@
-function result = BioFailed(errorCode)
-global p;
-global temp;
-result =  errorCode < Automation.BDaq.ErrorCode.Success && ...
-    errorCode >= Automation.BDaq.ErrorCode.ErrorHandleNotValid;
-
-end
